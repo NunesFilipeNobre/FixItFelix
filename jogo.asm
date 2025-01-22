e286a217ef1dbb9da2a07ca2075664cdd25ed85d
Filipe Nunes <nunesfilipenobre@gmail.com>
2025-01-22 12:40:54 -0300

    Troquei felix pelo amogus e adicionei o mapa
    tudo funcionando

--- jogo.asm
+++ jogo.asm
@@ -1,16 +1,19 @@
 .data
-.include "Assets/Sprites/predio.data"
-.include "Assets/Sprites/felixParado.data"
+.include "Assets/Sprites/amogus.data"
+.include "Assets/Sprites/chao.data"
+.include "Assets/Sprites/mapa.data"
 CHAR_POS:	.half 0,0			#Posição do personagem
 OLD_CHAR_POS:	.half 0,0			#Posição antiga do personagem (no ultimo frame)
 
 .text
 SETUP:		
-		la s0,predio			#Carrega endereço .data do predio
-		li s1,52			#Posição x = 52
-		li s2,24			#Posição y = 24
+		la s0,mapa			#Carrega endereço .data do mapa
+		li s1,0				#Posição x = 0
+		li s2,0				#Posição y = 0
 		li s3,0				#Frame 0
 		jal PRINT			#Função print
+		li s3,1				#Frame 1
+		jal PRINT			#Mapa gerado nos 2 frames
 
 GAME_LOOP:	jal KEY2			#Função KEY2
 		
@@ -18,18 +21,20 @@ GAME_LOOP:	jal KEY2			#Função KEY2
 
 		la t0,CHAR_POS			#Carrega posição do perosnagem CHAR_POS em t0
 
-		la s0,felixParado		#Carrega edereço do .data do felix parado
+		la s0,amogus			#Carrega edereço do .data do amogus
 		lh s1,0(t0)			#Posição x = primeiro half do CHAR_POS
 		lh s2,2(t0)			#Posição y = segundo half do CHAR_POS
 		mv s3,s4			#Frame = s4 (Fica trocando entre frame 0 e 1 por causa do xor acima)
 		jal PRINT			#Função print
 		
 		li t0,0xFF200604		#Endereço do frame a ser mostrado no bitmap display
-		sw s4,0(t0)			#Salva 1 ou 0 no frame
+		sw s4,0(t0)			#Salva 1 ou 0 no frame (troca pra esse frame)
+		
+		la t0,OLD_CHAR_POS		#Carrega posição antiga do perosnagem OLD_CHAR_POS em t0
 
-		la s0,predio			#Carrega endereço .data do predio
-		li s1,52			#Posição x = 52
-		li s2,24			#Posição y = 24
+		la s0,chao			#Carrega endereço .data do chao
+		lh s1,0(t0)			#Posição x = primeiro half do OLD_CHAR_POS
+		lh s2,2(t0)			#Posição y = segundo half do OLD_CHAR_POS
 		xori s3,s3,1			#Usa o frame oposto
 		jal PRINT			#Função print
 		
@@ -61,7 +66,7 @@ CHAR_ESQ:	la t0,CHAR_POS			#Carrega endereço da posição do personagem
  		sw t2,0(t1)			#Coloca os 2 na nova posição antiga
  		
  		lh t1,0(t0)			#Carrega o primeiro half word (x) da posição do personagem atual
- 		addi t1,t1,-4			#Diminui ele em 4
+ 		addi t1,t1,-32			#Diminui ele em 4
  		sh t1,0(t0)			#Atualiza a posição do personagem
  		ret				#Retorna
  		
@@ -71,7 +76,7 @@ CHAR_DIR:	la t0,CHAR_POS
  		sw t2,0(t1)
  		
  		lh t1,0(t0)
- 		addi t1,t1,4
+ 		addi t1,t1,32
  		sh t1,0(t0)
  		ret
  		
@@ -81,7 +86,7 @@ CHAR_CIMA:	la t0,CHAR_POS
  		sw t2,0(t1)
  		
  		lh t1,2(t0)
- 		addi t1,t1,-4
+ 		addi t1,t1,-32
  		sh t1,2(t0)
  		ret
  		
@@ -91,7 +96,7 @@ CHAR_BAIXO:	la t0,CHAR_POS
  		sw t2,0(t1)
  		
  		lh t1,2(t0)
- 		addi t1,t1,4
+ 		addi t1,t1,32
  		sh t1,2(t0)
  		ret
 #
@@ -108,40 +113,40 @@ CHAR_BAIXO:	la t0,CHAR_POS
 #	t3 = contador coluna
 #	t4 = largura
 #	t5 = altura
-PRINT:		li t0, 0xFF0	#Inicio do endereço do bitmap display
-		add t0,t0,s3	#0xFF0 ou 0xFF1 dependendo do frame s3
-		slli t0,t0,20	#Preenche o restante do endereço com zeros (0xFF000000 ou 0xFF100000)
+PRINT:		li t0, 0xFF0		#Inicio do endereço do bitmap display
+		add t0,t0,s3		#0xFF0 ou 0xFF1 dependendo do frame s3
+		slli t0,t0,20		#Preenche o restante do endereço com zeros (0xFF000000 ou 0xFF100000)
 		
-		add t0,t0,s1	#Coordenada x da imagem
+		add t0,t0,s1		#Coordenada x da imagem
 		
-		li t1,320	#Largura da tela
-		mul t1,t1,s2	#Largura da tela * y
-		add t0,t0,t1	#Endereço no bitmap dislay (x + y * largura da tela)
+		li t1,320		#Largura da tela
+		mul t1,t1,s2		#Largura da tela * y
+		add t0,t0,t1		#Endereço no bitmap dislay (x + y * largura da tela)
 		
-		addi t1,s0,8	#Endereço imagem (Pula os 2 bytes no inicio da imagem)
+		addi t1,s0,8		#Endereço imagem (Pula os 2 bytes no inicio da imagem)
 		
-		mv t2,zero	#Zera os contadores de linha
+		mv t2,zero		#Zera os contadores de linha
 		mv t3,zero
 		
-		lw t4,0(s0)	#Recebe a largura especificada no arquivo (primeiro byte)
-		lw t5,4(s0)	#Receba a altura especificada no arquivo (segundo byte)
+		lw t4,0(s0)		#Recebe a largura especificada no arquivo (primeiro byte)
+		lw t5,4(s0)		#Receba a altura especificada no arquivo (segundo byte)
 		
-PRINT_LINHA:	lw t6,0(t1)	#Pega 4 bytes (word) do arquivo da imagem
-		sw t6,0(t0)	#Salva 4 bytes (word) no endereço da bitmap display
+PRINT_LINHA:	lw t6,0(t1)		#Pega 4 bytes (word) do arquivo da imagem
+		sw t6,0(t0)		#Salva 4 bytes (word) no endereço da bitmap display
 		
-		addi t0,t0,4	#Pula pros proximos 4 bytes da imagem
-		addi t1,t1,4	#Pula pros proximos 4 bytes do display
+		addi t0,t0,4		#Pula pros proximos 4 bytes da imagem
+		addi t1,t1,4		#Pula pros proximos 4 bytes do display
 		
 		
-		addi t3,t3,4	#Soma 4 ao contador de coluna
+		addi t3,t3,4		#Soma 4 ao contador de coluna
 		blt t3,t4,PRINT_LINHA	#Se for menor que a largura da imagem, volta pro loop
 		
-		addi t0,t0,320	#Se não, pula para a proxima linha no bitmap display (soma 320 e subtrai a largura dele)
+		addi t0,t0,320		#Se não, pula para a proxima linha no bitmap display (soma 320 e subtrai a largura dele)
 		sub t0,t0,t4	
 		
-		mv t3,zero	#Zera o contador de coluna
-		addi t2,t2,1	#Soma 1 ao contador de linha
+		mv t3,zero		#Zera o contador de coluna
+		addi t2,t2,1		#Soma 1 ao contador de linha
 		bgt t5,t2,PRINT_LINHA	#Se a altura da imagem for maior que o contador de linha, volta pro loop
 		
-		ret		#return
+		ret			#return
 		
